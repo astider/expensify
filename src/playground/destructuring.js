30707c37a98510820d72045cf467cfbb584f67bc
astider@home <astider@gmail.com>
2018-02-17 18:36:14 +0700

    example of Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,5 +1,7 @@
 // console.log(`destructuring`)
 
+// Object Destructuring
+
 // const person = {
 //   name: 'Guy',
 //   age: 25,
@@ -17,14 +19,27 @@
 //   console.log(`It's ${temperature} degree in ${city}`)
 // }
 
-const book = {
-  name: 'Ego in the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-}
+// const book = {
+//   name: 'Ego in the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// }
 
-const { name: publisherName = 'Self-published' } = book.publisher
+// const { name: publisherName = 'Self-published' } = book.publisher
 
-console.log(publisherName)
+// console.log(publisherName)
+
+
+// Array Destructuring
+
+const address = ['49/116', 'Phattanakarn', 'Bangkok']
+const [, road, city, zip = 'not available'] = address
+
+console.log(`road name is ${road} and the road is in ${city}`)
+console.log(`zip code: ${zip}`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+const [name, , mediumPrice] = item
+console.log(`A medium ${name} costs ${mediumPrice}`)
