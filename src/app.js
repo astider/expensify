ac9593dbfd2af3b2d920c93c6897e798e54f15c2
astider@home <astider@gmail.com>
2018-02-19 01:26:58 +0700

    split redux store, learning HOC

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense, removeExpense, editExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const store = configureStore()
 
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: '500' }))
+store.dispatch(addExpense({ description: 'Gas Bill', amount: '900' }))
+store.dispatch(setTextFilter('ter'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
