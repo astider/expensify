180a4c63583ebc972b10d30b2963b2df7ba30ac9
Nontapat <astider@gmail.com>
2018-02-19 18:57:57 +0700

    working on ExpenseForm

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense, removeExpense, editExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
+import { setTextFilter, sortByAmount } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore()
 
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }))
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: '500' }))
-store.dispatch(addExpense({ description: 'Gas Bill', amount: '900' }))
-store.dispatch(setTextFilter('ter'))
+//store.dispatch(sortByAmount())
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import ExpenseList from './ExpenseList'
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboradPage = () => (
   <div>
-    This is Dashboard component
+    <ExpenseListFilters />
+    <ExpenseList />
   </div>
 )
 
